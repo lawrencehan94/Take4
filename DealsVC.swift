34aa7e4764be34fb1e11b6df3e0d36254bc76755
Lawrence <lawrencehan94@gmail.com>
2017-10-12 17:44:31 -0700

    v4

--- DealsVC.swift
+++ DealsVC.swift
@@ -1,14 +1,46 @@
 class Deal {
-  var name: String
-  var ev: Double
-  var evtorevenue: Double
-  var evtoebitda: Double
-  
-  init(name: String, ev: Double, evtorevenue: Double, evtoebitda: Double) {
-    self.name = name
-    self.ev = ev
-    self.evtorevenue = evtorevenue
-    self.evtoebitda = evtoebitda
-  }
-  
+	var name: String
+	var ev: Double
+	var ltmEVRev: Double
+	var ltmEVEBITDA: Double
+	
+	init(_ nameInput: String, _ evInput: Double, _ ltmEVRevInput: Double, _ ltmEVEBITDAInput: Double) {
+		name = nameInput
+		ev = evInput
+		ltmEVRev = ltmEVRevInput
+		ltmEVEBITDA = ltmEVEBITDAInput
+	}
 }
+
+var deals = [Deal]()
+
+func addDeals() {
+deals.append(Deal("Intacct / Sage", 500, 9.7, 14.5))
+}
+
+
+addDeals()
+
+var dealCount = 0.0
+var sum = 0.0
+var mean = 0.0
+
+for deal in deals {
+	print(deal.name)
+	print(deal.ev)
+	print(deal.ltmEVRev)
+	print(deal.ltmEVEBITDA)
+	dealCount += 1
+	sum += deal.ltmEVRev
+}
+
+func calculateMean() {
+	mean = sum / dealCount
+}
+
+calculateMean()
+
+print(" ")
+print("Deal Count: \(dealCount)")
+print("Sum: \(sum)")
+print("Mean: \(mean)")
