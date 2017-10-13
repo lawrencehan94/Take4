9092360295f3665a69e137b68ad5ed81f823592e
Lawrence <lawrencehan94@gmail.com>
2017-10-12 17:07:57 -0700

    v2

--- DealsVC.swift
+++ DealsVC.swift
@@ -1,1 +1,11 @@
-
+class Deal {
+  var name: String
+  var ev: Double
+  var evtorevenue: Double
+  var evtoebitda: Double
+  
+  init(name: String, ev: Double, evtorevenue: Double, evtoebitda: Double) {
+  
+  }
+  
+}
