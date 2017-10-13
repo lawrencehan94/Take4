7d6e6b29e842d481110e34ea7be0f561d52b5be6
Lawrence <lawrencehan94@gmail.com>
2017-10-12 17:54:31 -0700

    v4

--- DealsVC.swift
+++ DealsVC.swift
@@ -2,24 +2,24 @@ class Deal {
 	var name: String
 	var ev: Double
 	var ltmEVRev: Double
-	var ltmEVEBITDA: Double
 	
-	init(_ nameInput: String, _ evInput: Double, _ ltmEVRevInput: Double, _ ltmEVEBITDAInput: Double) {
+	init(_ nameInput: String, _ evInput: Double, _ ltmEVRevInput: Double) {
 		name = nameInput
 		ev = evInput
 		ltmEVRev = ltmEVRevInput
-		ltmEVEBITDA = ltmEVEBITDAInput
 	}
 }
 
 var deals = [Deal]()
 
-func addDeals() {
-deals.append(Deal("Intacct / Sage", 500, 9.7, 14.5))
+func addSoftwareDeals() {
+deals.append(Deal("Intacct / Sage", 500, 9.7))
+deals.append(Deal("HP / Autonomy", 1300, 13.5))
+
 }
 
 
-addDeals()
+addSoftwareDeals()
 
 var dealCount = 0.0
 var sum = 0.0
@@ -29,7 +29,7 @@ for deal in deals {
 	print(deal.name)
 	print(deal.ev)
 	print(deal.ltmEVRev)
-	print(deal.ltmEVEBITDA)
+	print(" ")
 	dealCount += 1
 	sum += deal.ltmEVRev
 }
@@ -40,7 +40,6 @@ func calculateMean() {
 
 calculateMean()
 
-print(" ")
 print("Deal Count: \(dealCount)")
 print("Sum: \(sum)")
 print("Mean: \(mean)")
